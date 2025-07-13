cf825ba11000c260529138760f15e9f529dafcfc
mPtrsn <malte.ptrsn@gmail.com>
2025-07-13 23:46:23 +0200

    added rest client as bean

--- src/main/java/com/malte/immochallenge/ImmochallengeApplication.java
+++ src/main/java/com/malte/immochallenge/ImmochallengeApplication.java
@@ -2,14 +2,21 @@ package com.malte.immochallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 @EnableScheduling
 public class ImmochallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImmochallengeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImmochallengeApplication.class, args);
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
+    }
 
 }
